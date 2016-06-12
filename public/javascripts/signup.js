bc6d2b1ca2fa535c8ee22cd158c3fe1f3998ddcb
andonilloret <andonilloret@gmail.com>
2016-06-12 16:33:08 -0400

    Login and Register views

---
+++ public/javascripts/signup.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+  $("#formSignup").submit( function( e ){
+    e.preventDefault();
+    alert("Estamos trabajando en este paso");
+  });
+});
+
+/*
+252525;
+3d9add;
+*/
